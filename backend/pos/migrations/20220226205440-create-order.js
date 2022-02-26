29b5dfee543fba63343e5370ab66699d9c759499
Winnie Li <leechangwook0621@gmail.com>
2022-02-26 16:11:32 -0500

    [add] order model

---
+++ backend/pos/migrations/20220226205440-create-order.js
@@ -0,0 +1,52 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      total_price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+      },
+      store_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        reference: {
+          model: 'store',
+          key: 'id',
+          as: 'store_id'
+        }
+      },
+      customer_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        reference: {
+          model: 'customer',
+          key: 'id',
+          as: 'customer_id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('orders');
+  }
+};
---
+++ backend/pos/models/order.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.store, {
+        foreignKey: 'store_id',
+        onDelete: 'CASADE'
+      });
+      this.belongsTo(models.customer, {
+        foreignKey: 'customer_id',
+        onDelete: 'CASADE'
+      });
+    }
+  }
+  order.init({
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    total_price: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'order',
+  });
+  return order;
+};
