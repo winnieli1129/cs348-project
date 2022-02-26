af048e0e68f1b53b079721772ead667f18625906
Winnie Li <leechangwook0621@gmail.com>
2022-02-26 17:26:34 -0500

    [add] order_product table migration

---
+++ backend/pos/migrations/20220226221035-create-order-product-table.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('order_product', {
+      product_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        onDelete: 'CASCADE',
+        reference: {
+          model: 'product',
+          key: 'id',
+          as: 'product_id'
+        },
+      },
+      order_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        onDelete: 'CASCADE',
+        reference: {
+          model: 'order',
+          key: 'id',
+          as: 'order_id'
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('order_product');
+  }
+};
