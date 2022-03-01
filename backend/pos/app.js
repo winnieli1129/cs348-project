44c0249a0b370afb1a3cddcdbfd5b761811f8f0b
Winnie Li <leechangwook0621@gmail.com>
2022-03-01 13:56:39 -0500

    [add] endpoint to create customer

--- backend/pos/app.js
+++ backend/pos/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var customerRegisterRouter = require('./routes/register_customer');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/customer-register', customerRegisterRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/pos/routes/register_customer.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const customer = require('../models').customer;
+
+/* POST customer registration. */
+router.post('/', async function(req, res, next) {
+  try {
+    const { name, email, phone_number, password } = req.body;
+
+    if (!(name && email && phone_number && password)) {
+      return res.status(400).send('Require name, email, phone_number, and password');
+    }
+
+    const oldUser = await customer.findOne({
+      where: {
+        email: email
+      }
+    });
+    if (oldUser) {
+      return res.status(409).send('User Already Exist. Please Login');
+    }
+
+    encryptedPassword = await bcrypt.hash(password, 10);
+    const user = await customer.create({
+      name: name,
+      email: email,
+      phone_number: phone_number,
+      password: encryptedPassword
+    });
+
+    return res.status(201).json(user);
+  } catch(err) {
+    console.log(err);
+  }
+});
+  
+module.exports = router;
