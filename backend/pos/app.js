0574a8dbaea994fb705e20b773ba4b58b31f2487
Winnie Li <leechangwook0621@gmail.com>
2022-03-01 15:15:34 -0500

    [add] endpoint to update customer

--- backend/pos/app.js
+++ backend/pos/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var customerRegisterRouter = require('./routes/register_customer');
 var deleteCustomerRouter = require('./routes/delete_customer');
+var updateCustomerRouter = require('./routes/update_customer');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/customer-register', customerRegisterRouter);
 app.use('/delete-customer', deleteCustomerRouter);
+app.use('/update-customer', updateCustomerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/pos/routes/update_customer.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const customer = require('../models').customer;
+
+/* POST update customer. */
+router.post('/', async function(req, res, next) {
+  try {
+    if (!req.body.hasOwnProperty('customer_id')) {
+      return res.status(400).send('Require customer_id');
+    }
+
+    const customer_id = req.body['customer_id'];
+    delete req.body['customer_id']
+    if (req.body.hasOwnProperty('password')) {
+      encryptedPassword = await bcrypt.hash(req.body['password'], 10);
+      req.body['password'] = encryptedPassword;
+    }
+
+    console.log(req.body);
+    
+    customer.update(req.body, {
+        where: {
+          id: customer_id
+        }
+    }).then(user => {
+      if (user == 0) {
+        return res.status(404).send({error: 'Customer not found'});
+      }
+
+      return res.status(200).send();
+    });
+
+  } catch(err) {
+    console.log(err);
+  }
+});
+  
+module.exports = router;
