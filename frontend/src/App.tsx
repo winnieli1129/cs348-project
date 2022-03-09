4a7e751e48f9273f19b42755218c91a7d9fe971f
Linda <lindachen30233@gmail.com>
2022-03-09 12:25:10 -0500

    browse inventory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,12 @@ import {
   theme,
 } from "@chakra-ui/react"
 import Login from './pages/Login'
+import BrowseInventory from "./pages/BrowseInventory"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box w="100vw" h="100vh">
-      <Login/>
+      <BrowseInventory/>
     </Box>
   </ChakraProvider>
 )
