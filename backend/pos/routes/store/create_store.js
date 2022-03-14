519e76c4b89d028faa8b25eb39ae5cf684d39036
Winnie Li <leechangwook0621@gmail.com>
2022-03-14 00:33:46 -0400

    [mod] fix indent

--- backend/pos/routes/store/create_store.js
+++ backend/pos/routes/store/create_store.js
@@ -5,37 +5,37 @@ const store = require('../../models').store;
 const auth = require('../../middleware/auth');
 
 /*POST create store. */
-router.store('/', auth, async function(req, res, next) {
-    try{
-        if(req.user.employee !== true){
-            return res.status(401).send('Unauthorized User');
-        }
+router.post('/', auth, async function(req, res, next) {
+  try{
+    if(req.user.employee !== true){
+        return res.status(401).send('Unauthorized User');
+    }
     
 
     const { address, phone_number } = req.body;
 
     if (!(address && phone_number)){
-        return res.status(400).send('Requires address and phone number');
+      return res.status(400).send('Requires address and phone_number');
     }
 
     const old_store = await store.findOne({
-        where: {
-            address: address
-        }
+      where: {
+        address: address
+      }
     });
     if (old_store) {
-        return res.status(409).send('Store already exists at that address.');
+      return res.status(409).send('Store already exists at that address.');
     }
 
     const new_store = await store.create({
-        address: address,
-        phone_number: phone_number
+      address: address,
+      phone_number: phone_number
     });
 
     return res.status(201).json(new_store);
-} catch(err) {
-    console.log(err);
-}
+  } catch(err) {
+      console.log(err);
+  }
 });
 
 module.exports = router;
