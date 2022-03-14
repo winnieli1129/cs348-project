ce2b0549db9f12be9d1c393f51a756b8d20714ad
Winnie Li <leechangwook0621@gmail.com>
2022-03-14 00:43:42 -0400

    [mod] fix get_stores

--- backend/pos/routes/store/get_stores.js
+++ backend/pos/routes/store/get_stores.js
@@ -1,24 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const { QueryTypes } = require('sequelize');
-const db = require('../../models/index');
 
-const product = require('../../models').product;
+const store = require('../../models').store;
 const auth = require('../../middleware/auth');
 
 /* GET retrieve all stores. */
 router.get('/', auth, async function(req, res, next){
-    try{
-        if(req.user.employee !== true) {
-            return res.status(401).send('Unauthorized User');
-        }
-
-        const s = await store.findAll();
-
-        return res.status(200).json(s);
-    } catch(err) {
-        console.log(err);
+  try{
+    if(req.user.employee !== true) {
+      return res.status(401).send('Unauthorized User');
     }
+
+    const s = await store.findAll();
+
+    return res.status(200).json(s);
+  } catch(err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
