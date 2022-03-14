16e4ebef26ab11bb1ed0d04aaf1663436e8b9c06
Winnie Li <leechangwook0621@gmail.com>
2022-03-14 00:46:02 -0400

    [mod] fix get_store

--- backend/pos/routes/store/get_store.js
+++ backend/pos/routes/store/get_store.js
@@ -1,32 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-const product = require('../../models').product;
+const store = require('../../models').store;
 const auth = require('../../middleware/auth');
 
 /* GET retrieve store with id/address. */
 router.get('/', auth, async function(req, res, next) {
-    try{
-        if(req.user.employee !== true){
-            return res.status(401).send('Unauthorized User');
-        }
-
-        if(!(req.body.hasOwnProperty('store_id') || req.body.hasOwnProperty('address'))){
-            return res.status(400).send('Require either store_id or address');
-        }
-        var searchCondition = req.body['store_id'] ? {id: req.body['store_id']} : {address: req.body['address']};
-
-        const s = await store.findOne({
-            where: searchCondition
-        });
-        if(!s) {
-            return res.status(404).send('Store does not exist.');
-        }
-
-        return res.status(200).json(s)
-    } catch(err) {
-        console.log(err);
+  try{
+    if(req.user.employee !== true){
+        return res.status(401).send('Unauthorized User');
     }
+
+    const { store_id } = req.body;
+
+    if(!store_id) {
+      return res.status(400).send('Require store_id');
+    }
+
+    const s = await store.findOne({
+      where: { id: store_id}
+    });
+    if(!s) {
+      return res.status(404).send('Store does not exist.');
+    }
+
+    return res.status(200).json(s)
+  } catch(err) {
+      console.log(err);
+  }
 });
 
 module.exports = router;
