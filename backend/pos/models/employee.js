934682fe9c891e13cdcb8057dd2f4e73dc2d8696
Winnie Li <leechangwook0621@gmail.com>
2022-03-14 00:33:22 -0400

    [mod] update models

--- backend/pos/models/employee.js
+++ backend/pos/models/employee.js
@@ -12,7 +12,8 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.belongsTo(models.store, {
-        foreignKey: 'store_id'
+        foreignKey: 'store_id',
+        onDelete: 'CASCADE'
       });
     }
   }
