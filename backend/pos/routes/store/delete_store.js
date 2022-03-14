2e9343ba95b4c41e16947eb94c7659e048cbfccb
Winnie Li <leechangwook0621@gmail.com>
2022-03-14 15:20:03 -0400

    [mo]d change delete_store to raw sql

--- backend/pos/routes/store/delete_store.js
+++ backend/pos/routes/store/delete_store.js
@@ -2,33 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 const store = require('../../models').store;
+const { QueryTypes } = require('sequelize');
+const db = require('../../models/index');
 const auth = require('../../middleware/auth');
 
 /* POST delete store. */
 router.post('/', auth, async function(req, res, next) {
-try{
-    if (req.user.employee !== true) {
-        return res.status(401).send('Unauthorized User');
-    }
+	try{
+		if (req.user.employee !== true) {
+			return res.status(401).send('Unauthorized User');
+		}
 
-    const { store_id } = req.body;
+		const { store_id } = req.body;
 
-    if(!store_id) {
-        return res.status(400).send('Require store_id');
-    }
+		if(!store_id) {
+			return res.status(400).send('Require store_id');
+		}
 
-    store.destroy({
-        where: { id: store_id }
-    }).then(store => {
-        if (!store) {
-            return res.status(404).send({error: 'Store not found'});
-        }
-
-        return res.status(200).send();
-    });
-} catch(err) {
-    console.log(err);
-}
+		await db.sequelize.query(`DELETE FROM \`stores\` WHERE \`id\`=:id;`, { replacements: {id: store_id}, type: QueryTypes.DELETE });
+		
+		return res.status(200).send();
+	} catch(err) {
+		console.log(err);
+	}
 });
 
 module.exports = router;
