45da39942b0b5320a544229772b5eff080ffcb4d
Winnie Li <leechangwook0621@gmail.com>
2022-03-15 02:42:17 -0400

    [mod] check customer not found condition

--- backend/pos/routes/customer/delete_customer.js
+++ backend/pos/routes/customer/delete_customer.js
@@ -15,24 +15,13 @@ router.post('/', auth, async function(req, res, next) {
       return res.status(400).send('Require customer_id');
     }
 
-    // TODO: change to raw sql
-    const user = await db.sequelize.query(`DELETE FROM \`customers\` WHERE \`id\`=:id;`, { replacements: {id: customer_id}, type: QueryTypes.DELETE });
-    if (!user) {
+    const user = await db.sequelize.query(`DELETE FROM \`customers\` WHERE \`id\`=:id;`, { replacements: {id: customer_id} });
+    if (user[0].affectedRows === 0) {
       return res.status(404).send({error: 'Customer not found'});
     }
 
     return res.status(200).send();
-
-    // customer.destroy({
-    //   where: {
-    //     id: customer_id
-    //   }
-    // }).then(user => {
-    //   if (!user) {
-    //   return res.status(404).send({error: 'Customer not found'});
-    // }
-    //   return res.status(200).send();
-    // });
+    
   } catch(err) {
     console.log(err);
   }
