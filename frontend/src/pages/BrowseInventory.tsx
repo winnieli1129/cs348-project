1b400a216e3532a85dcf99123d63d7c48a4af1f5
Linda <lindachen30233@gmail.com>
2022-03-18 18:38:32 -0400

    edit browse inventory

--- frontend/src/pages/BrowseInventory.tsx
+++ frontend/src/pages/BrowseInventory.tsx
@@ -21,12 +21,12 @@ import {
     IconButton,
     Collapse,
     useDisclosure,
-
-
+    Image,
+    Button,
 
 } from "@chakra-ui/react"
 
-import axios from 'axios';
+import Kermit from '/Users/chialin/Documents/CS348/cs348-project/frontend/src/kermit.jpeg'
 
 const Inventory = () => {
     const { isOpen, onToggle } = useDisclosure()
@@ -37,17 +37,19 @@ const Inventory = () => {
                 opacity: '90%',
                 color: "white",
             }}>
-                <Td>inches</Td>
-                <Td>millimetres (mm)</Td>
-                <Td isNumeric>25.4</Td>
+                <Td>123456</Td>
+                <Td>Apple</Td>
+                <Td isNumeric>$25.4</Td>
                 <Td isNumeric>10</Td>
                 <Td>
                     <Flex mr="-10">
                         <ButtonGroup variant='ghost' spacing='1'>
                             <IconButton onClick={onToggle} aria-label='Edit' icon={<EditIcon />} _hover={{
-                                color: "black" }} />
+                                color: "black"
+                            }} />
                             <IconButton aria-label='Delete' icon={<DeleteIcon />} _hover={{
-                                color: "black" }}/>
+                                color: "black"
+                            }} />
                         </ButtonGroup>
                     </Flex>
                 </Td>
@@ -55,11 +57,50 @@ const Inventory = () => {
 
             <Tr hidden={!isOpen}>
                 <Td colSpan={5}>
-                    <Flex h="400px" bg="grey">
-                        <Input
-                            variant='flushed'
-                            placeholder='serial number'
-                        />
+                    <Flex h="230px">
+                        <Image w="150px" h="150px" src={Kermit} />
+                        <Flex direction='column' w='100%' ml='20'>
+                            <Flex justifyContent='space-between'>
+                                <Flex direction='column' >
+                                    <Text fontSize='sm' mt='10' mb='3'>Serial Number</Text>
+                                    <Input
+                                        variant='flushed'
+                                        placeholder='123456'
+                                        w='150px'
+                                    />
+                                </Flex>
+                                <Flex direction='column' ml='30px'>
+                                    <Text fontSize='sm' mt='10' mb='3'>Name</Text>
+                                    <Input
+                                        variant='flushed'
+                                        placeholder='Apple'
+                                        w='150px'
+                                    />
+                                </Flex>
+                                <Flex direction='column' ml='30px'>
+                                    <Text fontSize='sm' mt='10' mb='3'>Price</Text>
+                                    <Input
+                                        variant='flushed'
+                                        placeholder='25.4'
+                                        w='150px'
+                                    />
+                                </Flex>
+                                <Flex direction='column' ml='30px'>
+                                    <Text fontSize='sm' mt='10' mb='3'>In Stock</Text>
+                                    <Input
+                                        variant='flushed'
+                                        placeholder='10'
+                                        w='150px'
+                                    />
+                                </Flex>
+                            </Flex>
+                            <ButtonGroup variant='outline' spacing='6' mt='10' justifyContent='flex-end'>
+                                <Button color='#EE852F'>Cancel</Button>
+                                <Button bg='#EE852F' color='white'>Save</Button>
+                            </ButtonGroup>
+                        </Flex>
+
+
                     </Flex>
                 </Td>
             </Tr>
@@ -89,17 +130,17 @@ const BrowseInventory = () => {
                         <Tr>
                             <Th>Serial Number</Th>
                             <Th>Name</Th>
-                            <Th isNumeric>multiply by</Th>
+                            <Th isNumeric>Price</Th>
                             <Th isNumeric>In Stock</Th>
                             <Th></Th>
                         </Tr>
                     </Thead>
                     <Tbody >
-                        <Inventory/>
-                        <Inventory/>
-                        <Inventory/>
-                        <Inventory/>
-                        <Inventory/>
+                        <Inventory />
+                        <Inventory />
+                        <Inventory />
+                        <Inventory />
+                        <Inventory />
                     </Tbody>
                 </Table>
             </Flex>
