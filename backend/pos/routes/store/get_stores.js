e0d72d589e80751660f7be0f4bf98e2330726ac0
Winnie Li <leechangwook0621@gmail.com>
2022-03-19 15:32:29 -0400

    [mod] remove get_stores auth

--- backend/pos/routes/store/get_stores.js
+++ backend/pos/routes/store/get_stores.js
@@ -3,15 +3,10 @@ var router = express.Router();
 
 const { QueryTypes } = require('sequelize');
 const db = require('../../models/index');
-const auth = require('../../middleware/auth');
 
 /* GET retrieve all stores. */
-router.get('/', auth, async function(req, res, next){
+router.get('/', async function(req, res, next){
   try{
-    if(req.user.employee !== true) {
-      return res.status(401).send('Unauthorized User');
-    }
-
     const s = await db.sequelize.query(`SELECT * FROM \`stores\`;`, { type: QueryTypes.SELECT });
 
     return res.status(200).json(s);
